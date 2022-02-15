6831626b9d2b8fa355476cd75130a47e41c0b7b0
iman2700 <imail.amiri@gmail.com>
2022-02-15 22:59:55 +0330

    end of the discount

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,44 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+        [HttpGet("{productName}",Name ="GetDiscount")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName) 
+        {
+            var coupon=await _repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody]Coupon coupon)
+        {
+            return Ok(await _repository.UpdateDiscount(coupon));
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);//go to the another action and get result
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _repository.DeleteDiscount(productName));
+        }
+    }
+}
